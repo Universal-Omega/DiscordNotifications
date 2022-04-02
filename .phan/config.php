3a82acdb4c39877d57b625c3c789e28a808d6eeb
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-02 15:34:27 -0600

    Create config.php

---
+++ .phan/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['directory_list'] = array_merge(
+	$cfg['directory_list'], [
+		// '../../extensions/Flow',
+	]
+);
+
+$cfg['exclude_analysis_directory_list'] = array_merge(
+	$cfg['exclude_analysis_directory_list'], [
+		// '../../extensions/Flow',
+	]
+);
+
+$cfg['suppress_issue_types'] = [
+	// https://github.com/phan/phan/issues/3420
+	// 'PhanAccessMethodInternal',
+];
+
+return $cfg;
