a9d92f5d6d7a337eb169f4d21ff45ec645e40516
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-06 14:25:50 -0600

    Update Hooks.php

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -419,7 +419,7 @@ class Hooks implements
 	 * @inheritDoc
 	 */
 	public function onManualLogEntryBeforePublish( $logEntry ): void {
-		$this->pushDiscordNotify( LogFormatter::newFromEntry( $logEntry )->getIRCActionText(), null, '' );
+		$this->pushDiscordNotify( LogFormatter::newFromEntry( $logEntry )->getIRCActionComment(), null, '' );
 	}
 
 	/**
