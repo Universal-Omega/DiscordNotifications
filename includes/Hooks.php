e198bf6a79ec06e2bd8b8bc4ed2af7c244766e90
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-10 10:47:07 -0600

    Update Hooks.php

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -11,6 +11,7 @@ use Exception;
 use ExtensionRegistry;
 use Flow\Collection\PostCollection;
 use Flow\Model\UUID;
+use LogEntry;
 use LogFormatter;
 use ManualLogEntry;
 use MediaWiki\Auth\Hook\LocalUserCreatedHook;
@@ -424,13 +425,234 @@ class Hooks implements
 			htmlspecialchars_decode(
 				$this->getDiscordUserText( $performer ) . ' ' . $logEntry->getFullType() . ' ' . preg_replace(
 					'/\[\[(.*?)\]\]/',
-					'[$1](' . ( $this->titleFactory->newFromText( '$1' ) ?? Title::newMainPage() )->getFullURL() . ')', false ),
+					'[$1](' . $this->titleFactory->newFromText( '$1' )->getFullURL() . ')', false ),
 					LogFormatter::newFromEntry( $logEntry )->getIRCActionComment()
 				)
 			),
 			$performer,
 			$logEntry->getType()
 		);
+	}
+
+	/**
+	 * @param LogEntry $logEntry
+	 * @return string
+	 */
+	public function getDiscordActionComment( LogEntry $logEntry ): string {
+		$actionComment = $this->getDiscordActionText( LogEntry $logEntry );
+		$comment = $logEntry->getComment();
+
+		if ( $comment != '' ) {
+			if ( $actionComment == '' ) {
+				$actionComment = $comment;
+			} else {
+				$actionComment .= wfMessage( 'colon-separator' )->inContentLanguage()->text() . $comment;
+			}
+		}
+
+		return $actionComment;
+	}
+
+	/**
+	 * @param LogEntry $logEntry
+	 * @return string
+	 */
+	public function getDiscordActionText( LogEntry $logEntry ): string {
+		$parameters = $logEntry->getParameters();
+
+		$target = $logEntry->getTarget()->getPrefixedText();
+		$text = '';
+
+		$contLang = MediaWikiServices::getInstance()->getContentLanguage();
+
+		switch ( $logEntry->getType() ) {
+			case 'move':
+				switch ( $logEntry->getSubtype() ) {
+					case 'move':
+						$movesource = $parameters['4::target'];
+						$text = wfMessage( '1movedto2' )
+							->rawParams( $target, $movesource )->inContentLanguage()->escaped();
+						break;
+					case 'move_redir':
+						$movesource = $parameters['4::target'];
+						$text = wfMessage( '1movedto2_redir' )
+							->rawParams( $target, $movesource )->inContentLanguage()->escaped();
+						break;
+					case 'move-noredirect':
+						break;
+					case 'move_redir-noredirect':
+						break;
+				}
+				break;
+
+			case 'delete':
+				switch ( $entry->getSubtype() ) {
+					case 'delete':
+						$text = wfMessage( 'deletedarticle' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+					case 'restore':
+						$text = wfMessage( 'undeletedarticle' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+
+			case 'patrol':
+				// https://github.com/wikimedia/mediawiki/commit/1a05f8faf78675dc85984f27f355b8825b43efff
+				// Create a diff link to the patrolled revision
+				if ( $entry->getSubtype() === 'patrol' ) {
+					$diffLink = htmlspecialchars(
+						wfMessage( 'patrol-log-diff', $parameters['4::curid'] )
+							->inContentLanguage()->text() );
+					$text = wfMessage( 'patrol-log-line', $diffLink, "[[$target]]", "" )
+						->inContentLanguage()->text();
+				} else {
+					// broken??
+				}
+				break;
+
+			case 'protect':
+				switch ( $entry->getSubtype() ) {
+					case 'protect':
+						$text = wfMessage( 'protectedarticle' )
+							->rawParams( $target . ' ' . $parameters['4::description'] )
+							->inContentLanguage()
+							->escaped();
+						break;
+					case 'unprotect':
+						$text = wfMessage( 'unprotectedarticle' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+					case 'modify':
+						$text = wfMessage( 'modifiedarticleprotection' )
+							->rawParams( $target . ' ' . $parameters['4::description'] )
+							->inContentLanguage()
+							->escaped();
+						break;
+					case 'move_prot':
+						$text = wfMessage( 'movedarticleprotection' )
+							->rawParams( $target, $parameters['4::oldtitle'] )->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+
+			case 'newusers':
+				switch ( $entry->getSubtype() ) {
+					case 'newusers':
+					case 'create':
+						$text = wfMessage( 'newuserlog-create-entry' )
+							->inContentLanguage()->escaped();
+						break;
+					case 'create2':
+					case 'byemail':
+						$text = wfMessage( 'newuserlog-create2-entry' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+					case 'autocreate':
+						$text = wfMessage( 'newuserlog-autocreate-entry' )
+							->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+
+			case 'upload':
+				switch ( $entry->getSubtype() ) {
+					case 'upload':
+						$text = wfMessage( 'uploadedimage' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+					case 'overwrite':
+					case 'revert':
+						$text = wfMessage( 'overwroteimage' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+
+			case 'rights':
+				if ( count( $parameters['4::oldgroups'] ) ) {
+					$oldgroups = implode( ', ', $parameters['4::oldgroups'] );
+				} else {
+					$oldgroups = wfMessage( 'rightsnone' )->inContentLanguage()->escaped();
+				}
+				if ( count( $parameters['5::newgroups'] ) ) {
+					$newgroups = implode( ', ', $parameters['5::newgroups'] );
+				} else {
+					$newgroups = wfMessage( 'rightsnone' )->inContentLanguage()->escaped();
+				}
+				switch ( $entry->getSubtype() ) {
+					case 'rights':
+						$text = wfMessage( 'rightslogentry' )
+							->rawParams( $target, $oldgroups, $newgroups )->inContentLanguage()->escaped();
+						break;
+					case 'autopromote':
+						$text = wfMessage( 'rightslogentry-autopromote' )
+							->rawParams( $target, $oldgroups, $newgroups )->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+
+			case 'merge':
+				$text = wfMessage( 'pagemerge-logentry' )
+					->rawParams( $target, $parameters['4::dest'], $parameters['5::mergepoint'] )
+					->inContentLanguage()->escaped();
+				break;
+
+			case 'block':
+				switch ( $entry->getSubtype() ) {
+					case 'block':
+						// Keep compatibility with extensions by checking for
+						// new key (5::duration/6::flags) or old key (0/optional 1)
+						if ( $entry->isLegacy() ) {
+							$rawDuration = $parameters[0];
+							$rawFlags = $parameters[1] ?? '';
+						} else {
+							$rawDuration = $parameters['5::duration'];
+							$rawFlags = $parameters['6::flags'];
+						}
+						$duration = $contLang->translateBlockExpiry(
+							$rawDuration,
+							null,
+							(int)wfTimestamp( TS_UNIX, $entry->getTimestamp() )
+						);
+						$flags = BlockLogFormatter::formatBlockFlags( $rawFlags, $contLang );
+						$text = wfMessage( 'blocklogentry' )
+							->rawParams( $target, $duration, $flags )->inContentLanguage()->escaped();
+						break;
+					case 'unblock':
+						$text = wfMessage( 'unblocklogentry' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+					case 'reblock':
+						$duration = $contLang->translateBlockExpiry(
+							$parameters['5::duration'],
+							null,
+							(int)wfTimestamp( TS_UNIX, $entry->getTimestamp() )
+						);
+						$flags = BlockLogFormatter::formatBlockFlags( $parameters['6::flags'],
+							$contLang );
+						$text = wfMessage( 'reblock-logentry' )
+							->rawParams( $target, $duration, $flags )->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+
+			case 'import':
+				switch ( $entry->getSubtype() ) {
+					case 'upload':
+						$text = wfMessage( 'import-logentry-upload' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+					case 'interwiki':
+						$text = wfMessage( 'import-logentry-interwiki' )
+							->rawParams( $target )->inContentLanguage()->escaped();
+						break;
+				}
+				break;
+		}
+
+		return $text;
 	}
 
 	/**
