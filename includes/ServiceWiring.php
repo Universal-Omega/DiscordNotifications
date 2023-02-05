d0c0215faee5d17804dfea52f0bf2b75c0c3715d
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-05 14:41:55 -0700

    Update ServiceWiring.php

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -6,13 +6,8 @@ use MediaWiki\Extension\DiscordNotifications\DiscordNotifier;
 use MediaWiki\MediaWikiServices;
 
 return [
-	'DiscordEmbedBuilder' => static function ( MediaWikiServices $services ): DiscordEmbedBuilder {
-		return new DiscordEmbedBuilder();
-	},
-
 	'DiscordNotifier' => static function ( MediaWikiServices $services ): DiscordNotifier {
 		return new DiscordNotifier(
-			$services->getService( 'DiscordEmbedBuilder' ),
 			RequestContext::getMain(),
 			new ServiceOptions(
 				DiscordNotifier::CONSTRUCTOR_OPTIONS,
