f1b47a8d1575574fdf4422340b9306e0e48b2fd7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-05 14:18:21 -0700

    Update DiscordNotifier.php

--- includes/DiscordNotifier.php
+++ includes/DiscordNotifier.php
@@ -7,6 +7,9 @@ namespace MediaWiki\Extension\DiscordNotifications;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Permissions\PermissionManager;
 use MediaWiki\User\UserIdentity;
+use MessageLocalizer;
+use Title;
+use WikiPage;
 
 class DiscordNotifier {
 
@@ -14,15 +17,32 @@ class DiscordNotifier {
 		'DiscordAdditionalIncomingWebhookUrls',
 		'DiscordAvatarUrl',
 		'DiscordCurlProxy',
+		'DiscordExcludeNotificationsFrom',
 		'DiscordExcludedPermission',
 		'DiscordFromName',
+		'DiscordIncludePageUrls',
+		'DiscordIncludeUserUrls',
 		'DiscordIncomingWebhookUrl',
+		'DiscordNotificationWikiUrl',
+		'DiscordNotificationWikiUrlEnding',
+		'DiscordNotificationWikiUrlEndingBlockUser',
+		'DiscordNotificationWikiUrlEndingDeleteArticle',
+		'DiscordNotificationWikiUrlEndingDiff',
+		'DiscordNotificationWikiUrlEndingEditArticle',
+		'DiscordNotificationWikiUrlEndingHistory',
+		'DiscordNotificationWikiUrlEndingUserContributions',
+		'DiscordNotificationWikiUrlEndingUserPage',
+		'DiscordNotificationWikiUrlEndingUserRights',
+		'DiscordNotificationWikiUrlEndingUserTalkPage',
 		'DiscordSendMethod',
 		'Sitename',
 	];
 
 	/** @var DiscordEmbedBuilder */
 	private $discordEmbedBuilder;
+
+	/** @var MessageLocalizer */
+	private $messageLocalizer;
 
 	/** @var ServiceOptions */
 	private $options;
@@ -32,18 +52,20 @@ class DiscordNotifier {
 
 	/**
 	 * @param DiscordEmbedBuilder $discordEmbedBuilder
+	 * @param MessageLocalizer $messageLocalizer
 	 * @param ServiceOptions $options
 	 * @param PermissionManager $permissionManager
 	 */
 	public function __construct(
 		DiscordEmbedBuilder $discordEmbedBuilder,
+		MessageLocalizer $messageLocalizer,
 		ServiceOptions $options,
 		PermissionManager $permissionManager
 	) {
 		$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
 
 		$this->discordEmbedBuilder = $discordEmbedBuilder;
-
+		$this->messageLocalizer = $messageLocalizer;
 		$this->options = $options;
 		$this->permissionManager = $permissionManager;
 	}
@@ -210,5 +232,137 @@ class DiscordNotifier {
 
 		$context = stream_context_create( $extraData );
 		$result = file_get_contents( $url, false, $context );
+	}
+
+	/**
+	 * Replaces some special characters on urls. This has to be done as Discord webhook api does not accept urlencoded text.
+	 *
+	 * @param string $url
+	 * @return string
+	 */
+	public function parseurl( string $url ): string {
+		$url = str_replace( ' ', '%20', $url );
+		$url = str_replace( '(', '%28', $url );
+		$url = str_replace( ')', '%29', $url );
+
+		return $url;
+	}
+
+	/**
+	 * Gets nice HTML text for user containing the link to user page
+	 * and also links to user site, groups editing, talk and contribs pages.
+	 *
+	 * @param UserIdentity $user UserIdentity or if CentralAuth is installed, CentralAuthGroupMembershipProxy
+	 * @return string
+	 */
+	public function getDiscordUserText( $user ): string {
+		$userName = $user->getName();
+		$user_url = str_replace( '&', '%26', $userName );
+
+		$userName = str_replace( '>', '\>', $userName );
+
+		if ( $this->options->get( 'DiscordIncludeUserUrls' ) ) {
+			return sprintf(
+				'%s (%s | %s | %s | %s)',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $this->options->get( 'DiscordNotificationWikiUrlEndingUserPage' ) . $user_url ) . '|' . $userName . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $this->options->get( 'DiscordNotificationWikiUrlEndingBlockUser' ) . $user_url ) . '|' . $this->getMessage( 'discordnotifications-block' ) . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $this->options->get( 'DiscordNotificationWikiUrlEndingUserRights' ) . $user_url ) . '|' . $this->getMessage( 'discordnotifications-groups' ) . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $this->options->get( 'DiscordNotificationWikiUrlEndingUserTalkPage' ) . $user_url ) . '|' . $this->getMessage( 'discordnotifications-talk' ) . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $this->options->get( 'DiscordNotificationWikiUrlEndingUserContributions' ) . $user_url ) . '|' . $this->getMessage( 'discordnotifications-contribs' ) . '>'
+			);
+		} else {
+			return '<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $this->options->get( 'DiscordNotificationWikiUrlEndingUserPage' ) . $user_url ) . '|' . $userName . '>';
+		}
+	}
+
+	/**
+	 * Gets nice HTML text for article containing the link to article page
+	 * and also into edit, delete and article history pages.
+	 *
+	 * @param WikiPage $wikiPage
+	 * @param bool $diff
+	 * @return string
+	 */
+	public function getDiscordArticleText( WikiPage $wikiPage, bool $diff = false ): string {
+		$title = $wikiPage->getTitle()->getFullText();
+		$title_url = str_replace( '&', '%26', $title );
+		$prefix = '<' . $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $title_url;
+
+		if ( $this->options->get( 'DiscordIncludePageUrls' ) ) {
+			$out = sprintf(
+				'%s (%s | %s | %s',
+				$this->parseurl( $prefix ) . '|' . $title . '>',
+				$this->parseurl( $prefix . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingEditArticle' ) ) . '|' . $this->getMessage( 'discordnotifications-edit' ) . '>',
+				$this->parseurl( $prefix . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingDeleteArticle' ) ) . '|' . $this->getMessage( 'discordnotifications-delete' ) . '>',
+				$this->parseurl( $prefix . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingHistory' ) ) . '|' . $this->getMessage( 'discordnotifications-history' ) . '>'
+			);
+
+			if ( $diff ) {
+				$revisionId = $wikiPage->getRevisionRecord()->getId();
+
+				$out .= ' | ' . $this->parseurl( $prefix . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingDiff' ) . $revisionId ) . '|' . $this->getMessage( 'discordnotifications-diff' ) . '>)';
+			} else {
+				$out .= ')';
+			}
+
+			return $out . "\n";
+		} else {
+			return $this->parseurl( $prefix ) . '|' . $title . '>';
+		}
+	}
+
+	/**
+	 * Gets nice HTML text for title object containing the link to article page
+	 * and also into edit, delete and article history pages.
+	 *
+	 * @param Title $title
+	 * @return string
+	 */
+	public function getDiscordTitleText( Title $title ): string {
+		$titleName = $title->getFullText();
+		$title_url = str_replace( '&', '%26', $titleName );
+
+		if ( $this->options->get( 'DiscordIncludePageUrls' ) ) {
+			return sprintf(
+				'%s (%s | %s | %s)',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $title_url ) . '|' . $titleName . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $title_url . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingEditArticle' ) ) . '|' . $this->getMessage( 'discordnotifications-edit' ) . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $title_url . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingDeleteArticle' ) ) . '|' . $this->getMessage( 'discordnotifications-delete' ) . '>',
+				'<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $title_url . '&' . $this->options->get( 'DiscordNotificationWikiUrlEndingHistory' ) ) . '|' . $this->getMessage( 'discordnotifications-history' ) . '>'
+			);
+		} else {
+			return '<' . $this->parseurl( $this->options->get( 'DiscordNotificationWikiUrl' ) . $this->options->get( 'DiscordNotificationWikiUrlEnding' ) . $title_url ) . '|' . $titleName . '>';
+		}
+	}
+
+	/**
+	 * Returns whether the given title should be excluded
+	 *
+	 * @param string $title
+	 * @return bool
+	 */
+	public function titleIsExcluded( string $title ): bool {
+		if ( is_array( $this->options->get( 'DiscordExcludeNotificationsFrom' ) ) && count( $this->options->get( 'DiscordExcludeNotificationsFrom' ) ) > 0 ) {
+			foreach ( $this->options->get( 'DiscordExcludeNotificationsFrom' ) as &$currentExclude ) {
+				if ( strpos( $title, $currentExclude ) === 0 ) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param string $key
+	 * @param string ...$params
+	 * @return string
+	 */
+	public function getMessage( string $key, string ...$params ): string {
+		if ( $params ) {
+			return $this->messageLocalizer->msg( $key, ...$params )->inContentLanguage()->text();
+		} else {
+			return $this->messageLocalizer->msg( $key )->inContentLanguage()->text();
+		}
 	}
 }
