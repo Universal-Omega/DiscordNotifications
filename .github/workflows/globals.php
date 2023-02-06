dbe92605151da20970786c51fac62cddbb9b691a
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-05 17:13:21 -0700

    Update globals.php

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -2,3 +2,6 @@
 
 $wgDiscordIncomingWebhookUrl = 'https://discord.com/api/webhooks/960979188364484658/pejDt4WX1MT3D-JBg7l1LiKtZF3vAj8Nx7QnAKQOjbDJX9STNNdv5ugARCs3fVgLh_6N';
 $wgDiscordNotificationWikiUrl = 'http://localhost/';
+
+$wgDiscordExperimentalWebhook = 'https://discord.com/api/webhooks/963568043320041502/cy764BVVAJokojgQEke7XPMm4-vxrp6Coa9R5Buclpq6Hu5EHao1ideCHddNk2z1rPX5'
+$wgDiscordEnableExperimentalCVTFeatures = true;
