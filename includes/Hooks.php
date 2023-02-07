2163e3cf91c03ca91b6172bede47198a5a4bae53
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-06 18:23:15 -0700

    Update Hooks.php

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -134,6 +134,18 @@ class Hooks implements
 		}
 
 		if ( $isNew ) {
+			$message = $this->discordNotifier->getMessage( 'discordnotifications-article-created',
+				$this->discordNotifier->getDiscordUserText( $user ),
+				$this->discordNotifier->getDiscordArticleText( $wikiPage ),
+				$summary == '' ? '' : wfMessage( 'discordnotifications-summary' )->plaintextParams( $summary )->inContentLanguage()->text()
+			);
+
+			if ( $this->config->get( 'DiscordIncludeDiffSize' ) ) {
+				$message .= ' (' . $this->discordNotifier->getMessage( 'discordnotifications-bytes', sprintf( '%d', $rev->getSize() ) ) . ')';
+			}
+
+			$this->discordNotifier->notify( $message, $user, 'article_inserted' );
+
 			if ( $enableExperimentalCVTFeatures ) {
 				$regex = '/' . implode( '|', $this->config->get( 'DiscordExperimentalCVTMatchFilter' ) ) . '/';
 
@@ -166,18 +178,6 @@ class Hooks implements
 					], $this->config->get( 'DiscordExperimentalWebhook' ) );
 				}
 			}
-
-			$message = $this->discordNotifier->getMessage( 'discordnotifications-article-created',
-				$this->discordNotifier->getDiscordUserText( $user ),
-				$this->discordNotifier->getDiscordArticleText( $wikiPage ),
-				$summary == '' ? '' : wfMessage( 'discordnotifications-summary' )->plaintextParams( $summary )->inContentLanguage()->text()
-			);
-
-			if ( $this->config->get( 'DiscordIncludeDiffSize' ) ) {
-				$message .= ' (' . $this->discordNotifier->getMessage( 'discordnotifications-bytes', sprintf( '%d', $rev->getSize() ) ) . ')';
-			}
-
-			$this->discordNotifier->notify( $message, $user, 'article_inserted' );
 		} else {
 			$isMinor = (bool)$rev->isMinor();
 
@@ -185,6 +185,25 @@ class Hooks implements
 			if ( $isMinor && $this->config->get( 'DiscordIgnoreMinorEdits' ) ) {
 				return;
 			}
+
+			$message = $this->discordNotifier->getMessage(
+				'discordnotifications-article-saved',
+				$this->discordNotifier->getDiscordUserText( $user ),
+				$isMinor ? $this->discordNotifier->getMessage( 'discordnotifications-article-saved-minor-edits' ) : $this->discordNotifier->getMessage( 'discordnotifications-article-saved-edit' ),
+				$this->discordNotifier->getDiscordArticleText( $wikiPage, true ),
+				$summary == '' ? '' : wfMessage( 'discordnotifications-summary' )->plaintextParams( $summary )->inContentLanguage()->text()
+			);
+
+			if (
+				$this->config->get( 'DiscordIncludeDiffSize' ) &&
+				$this->revisionLookup->getPreviousRevision( $rev )
+			) {
+				$message .= ' (' . $this->discordNotifier->getMessage( 'discordnotifications-bytes',
+					sprintf( '%+d', $rev->getSize() - $this->revisionLookup->getPreviousRevision( $rev )->getSize() )
+				) . ')';
+			}
+
+			$this->discordNotifier->notify( $message, $user, 'article_saved' );
 
 			if ( $enableExperimentalCVTFeatures ) {
 				$regex = '/' . implode( '|', $this->config->get( 'DiscordExperimentalCVTMatchFilter' ) ) . '/';
@@ -234,25 +253,6 @@ class Hooks implements
 					], $this->config->get( 'DiscordExperimentalWebhook' ) );
 				}
 			}
-
-			$message = $this->discordNotifier->getMessage(
-				'discordnotifications-article-saved',
-				$this->discordNotifier->getDiscordUserText( $user ),
-				$isMinor ? $this->discordNotifier->getMessage( 'discordnotifications-article-saved-minor-edits' ) : $this->discordNotifier->getMessage( 'discordnotifications-article-saved-edit' ),
-				$this->discordNotifier->getDiscordArticleText( $wikiPage, true ),
-				$summary == '' ? '' : wfMessage( 'discordnotifications-summary' )->plaintextParams( $summary )->inContentLanguage()->text()
-			);
-
-			if (
-				$this->config->get( 'DiscordIncludeDiffSize' ) &&
-				$this->revisionLookup->getPreviousRevision( $rev )
-			) {
-				$message .= ' (' . $this->discordNotifier->getMessage( 'discordnotifications-bytes',
-					sprintf( '%+d', $rev->getSize() - $this->revisionLookup->getPreviousRevision( $rev )->getSize() )
-				) . ')';
-			}
-
-			$this->discordNotifier->notify( $message, $user, 'article_saved' );
 		}
 	}
 
