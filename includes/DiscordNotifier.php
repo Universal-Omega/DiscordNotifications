5270e2dbdc6b503261036aa6ab7b0555c8651356
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-06 21:27:20 -0700

    Update DiscordNotifier.php

--- includes/DiscordNotifier.php
+++ includes/DiscordNotifier.php
@@ -168,8 +168,8 @@ class DiscordNotifier {
 		$response = null;
 		$retries = 0;
 
-		// keep retrying
-		while ( true ) {
+		// Retry up to 5 times if hitting rate limit
+		while ( $retries <= 5 ) {
 			$h = curl_init();
 			curl_setopt( $h, CURLOPT_URL, $url );
 
@@ -194,7 +194,7 @@ class DiscordNotifier {
 
 			curl_close( $h );
 
-			if ( $retries >= 1000000000 && ( $curl_output === false || $status_code !== 200 && $status_code !== 204 ) ) {
+			if ( $retries >= 5 && ( $curl_output === false || $status_code !== 200 && $status_code !== 204 ) ) {
 				throw new Exception( 'cURL request failed with error: ' . $curl_output . ' and status code: ' . $status_code );
 			}
 
