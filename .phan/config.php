677db9de9abca3993a0c49a2d57da4ad09691c58
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-09 12:45:45 -0700

    phan: exclude CentralAuth

--- .phan/config.php
+++ .phan/config.php
@@ -4,12 +4,14 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
+		'../../extensions/CentralAuth',
 		'../../extensions/Flow',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], [
+		'../../extensions/CentralAuth',
 		'../../extensions/Flow',
 	]
 );
