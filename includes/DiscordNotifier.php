86c47c1186bc71a45f218b0569eff78d0f842093
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-08 18:42:07 -0700

    Update DiscordNotifier.php

--- includes/DiscordNotifier.php
+++ includes/DiscordNotifier.php
@@ -89,7 +89,7 @@ class DiscordNotifier {
 			$discordFromName = $this->options->get( 'Sitename' );
 		}
 
-		$message = preg_replace( '~<(http[^|]*)\|([^>]*)(>)?~', '[$2]($1)$3', $message );
+		$message = preg_replace( '~(<)(http)([^|]*)(\|)([^\>]*)(>)~', '[$5$6]($2$3)', $message );
 		$message = str_replace( [ "\r", "\n" ], '', $message );
 
 		switch ( $action ) {
