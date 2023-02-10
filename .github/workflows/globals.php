f83f89f7796bb29675829ba2da05629ab4ab4f19
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-10 12:32:57 -0700

    Use GitHub secrets for Discord webhooks

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -1,6 +1,6 @@
 <?php
 
-$wgDiscordIncomingWebhookUrl = 'https://discord.com/api/webhooks/960979188364484658/pejDt4WX1MT3D-JBg7l1LiKtZF3vAj8Nx7QnAKQOjbDJX9STNNdv5ugARCs3fVgLh_6N';
+$wgDiscordIncomingWebhookUrl = getenv( 'DISCORD_WEBHOOK' );
 $wgDiscordNotificationWikiUrl = 'http://localhost/';
 
 $wgDiscordNotificationCentralAuthWikiUrl = 'http://localhost/';
