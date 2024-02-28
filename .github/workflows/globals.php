e0cff98a1457e427fcc5cd727e671d8f0ae138e0
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-28 15:55:53 -0700

    Set $wgDiscordExperimentalFeedLanguageCode

--- .github/workflows/globals.php
+++ .github/workflows/globals.php
@@ -7,6 +7,7 @@ $wgDiscordNotificationCentralAuthWikiUrl = 'http://localhost/';
 
 $wgDiscordExperimentalWebhook = 'https://discord.com/api/webhooks/1078075188580720810/WeTY7r2qQ08XitYURvdP' . 'iTJ3aQAikqQPvk_Crb60xP24TB5UYMyJdFg9HDg7CYo8oktu';
 $wgDiscordEnableExperimentalCVTFeatures = true;
+$wgDiscordExperimentalFeedLanguageCode = 'en';
 
 $wgDiscordExperimentalCVTMatchFilter = [
 	'test',
